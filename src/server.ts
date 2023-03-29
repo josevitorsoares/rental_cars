9817a67bfc4ef026450d4f011a4b5597747ca63a
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-03-28 21:35:06 -0300

    Add initial settings for TypeORM

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import { router } from './routes';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from '../src/swagger.json';
 
+import "./database"
+
 const app = express();
 
 app.use(express.json());
