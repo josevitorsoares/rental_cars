bb308b1fa116c848abeafdce867fa0ff43015373
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-03-30 14:36:45 -0300

    Add Migration Create Categories

---
+++ src/database/MigrationsOrmconfig.ts
@@ -0,0 +1,25 @@
+import { DataSource } from "typeorm";
+
+const MigrationsSource = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5433,
+    username: 'admin_db',
+    password: 'rentalcars',
+    database: 'rentalCars_db',
+    logging: false,
+    synchronize: false,
+    name: 'default',
+    migrations: ['./src/database/migrations/**/*.ts'],
+});
+
+MigrationsSource
+    .initialize()
+    .then(() => {
+        console.log('Data Source has been inicialized');
+    })
+    .catch((err) => {
+        console.error('Data Source initialization', err)
+    });
+
+export default MigrationsSource;
