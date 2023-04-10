a24513d58d5436adcef36903c73c778d5c95eb55
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-04-10 07:07:56 -0300

    Add Migration Create User and User Entity

--- config/ormconfig.ts
+++ config/ormconfig.ts
@@ -1,6 +1,7 @@
 import { DataSource } from "typeorm";
 import { Category } from "../src/modules/cars/entities/Category";
 import { Specification } from "../src/modules/cars/entities/Specification";
+import { User } from "../src/modules/accounts/entities/User";
 
 const connectionSource = new DataSource({
     migrationsTableName: 'migrations',
@@ -10,7 +11,7 @@ const connectionSource = new DataSource({
     username: 'admin_db',
     password: 'rentalcars',
     database: 'rentalCars_db',
-    entities: [Category, Specification],
+    entities: [Category, Specification, User],
     synchronize: false,
 });
 
@@ -20,7 +21,7 @@ connectionSource
         console.log('Data Source has been inicialized');
     })
     .catch((err) => {
-        console.error('Data Source initialization', err)
+        console.error('Data Source initialization', err);
     });
 
 export default connectionSource
