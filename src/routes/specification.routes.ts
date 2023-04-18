bc3b824cb39bad130ca7188b82045f8834f32ed8
josevitorsoares <josevitorgoncalvessoares@gmail.com>
2023-04-18 07:04:07 -0300

    Using authentication middleware

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController';
 import { ListSpecificationsController } from '../modules/cars/useCases/listSpecifications/ListSpecificationsController';
+import { ensureAuthenticated } from '../middlewares/ensureAutheticated';
 
 const specificationRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationController = new ListSpecificationsController();
 
+specificationRoutes.use(ensureAuthenticated);
 specificationRoutes.post('/', createSpecificationController.handle);
 
 specificationRoutes.get('/', listSpecificationController.handle);
